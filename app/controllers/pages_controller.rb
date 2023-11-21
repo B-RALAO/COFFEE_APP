cc86261b14d5b65fb3d40bc7b617c732c8edecbf
Kirsty Mason <kirsty_mason9@hotmail.com>
2023-11-21 09:08:21 +0000

    Go to coffee index once logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    if user_signed_in?
-      render "coffees/index", status: :unprocessable_entity
-    end
+    render "coffees/index", status: :unprocessable_entity if user_signed_in?
   end
 end
