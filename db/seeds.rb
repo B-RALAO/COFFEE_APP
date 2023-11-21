605e4b76c28d92d7a05ef4f57afc4435802a7f1e
Joao Sagorro <joao.sagorro@gmail.com>
2023-11-21 12:40:06 +0000

    Seeds are already created, just run db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,35 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts 'Cleaning User DataBase'
+User.destroy_all
+
+puts 'Cleaning Coffee DataBase'
+Coffee.destroy_all
+
+puts 'Creating users for you'
+
+10.times do
+  users = User.new({
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: '123456789'
+  })
+  users.save!
+  puts 'User created'
+
+  puts 'Creating coffees'
+
+  rand(1..5).times do
+    coffee = Coffee.new({
+      name: Faker::Coffee.blend_name,
+      origin: Faker::Coffee.origin,
+      strength: (1..5).to_a.sample,
+      description: Faker::Coffee.notes,
+      rating: Faker::Number.decimal(l_digits: 1, r_digits: 2)
+    })
+    coffee.user = users
+    coffee.save!
+    puts 'Coffee created'
+    end
+end
+
+puts 'Finished creating coffees'
