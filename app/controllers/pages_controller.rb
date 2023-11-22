2657d2f041ad9f0eafd01ecbe7665c728602dca5
Bernardo Ralao <bernralao@gmail.com>
2023-11-22 12:24:48 +0000

    mergin landing and seeds

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @coffees = Coffee.all
+    @coffees = Coffee.limit(4).order(rating: :desc)
   end
 end
