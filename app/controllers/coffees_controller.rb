0014d3d3b754fc02c51e5ebd7a98353682a3fbfd
Kirsty Mason <kirsty_mason9@hotmail.com>
2023-11-23 18:19:37 +0000

    map styling

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -46,6 +46,17 @@ class CoffeesController < ApplicationController
     redirect_to coffees_path
   end
 
+  def map
+    @coffees = Coffee.all
+    @markers = @coffees.geocoded.map do |coffee|
+      {
+        lat: coffee.latitude,
+        lng: coffee.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {coffee: coffee}),
+      }
+    end
+  end
+
   private
 
   def set_coffee
