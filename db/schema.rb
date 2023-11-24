418220ab3b994c6d13d748fb938353783b8f3f0b
Kirsty Mason <kirsty_mason9@hotmail.com>
2023-11-24 12:33:10 +0000

    fixed dropdown

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2023_11_23_111839) do
-
 ActiveRecord::Schema[7.1].define(version: 2023_11_23_155623) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +60,14 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_155623) do
     t.index ["user_id"], name: "index_coffees_on_user_id"
   end
 
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
+  end
 
   create_table "purchases", force: :cascade do |t|
     t.integer "quantity", default: 0
@@ -74,15 +78,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_155623) do
     t.datetime "updated_at", null: false
     t.index ["coffee_id"], name: "index_purchases_on_coffee_id"
     t.index ["user_id"], name: "index_purchases_on_user_id"
-
-  create_table "pg_search_documents", force: :cascade do |t|
-    t.text "content"
-    t.string "searchable_type"
-    t.bigint "searchable_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
-
   end
 
   create_table "users", force: :cascade do |t|
